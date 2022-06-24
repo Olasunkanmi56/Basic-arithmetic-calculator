78755b3777c25d2193ca3bf64b5393503cb7dc07
Olasunkanmi56 <olasunkanmiolamide46@gmail.com>
2022-06-25 00:35:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// program for a simple calculator
+let result;
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const number1 = parseInt(prompt('Enter first number: '));
+const number2 = parseInt(prompt('Enter second number: '));
+
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
